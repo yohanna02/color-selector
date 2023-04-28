6eb9692edcff492e5a042292dcfa9223bbf656d9
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-28 16:39:57 +0100

    second commit

--- main.js
+++ main.js
@@ -6,21 +6,61 @@ const colorInput = document.querySelector("#color");
 
 let selectedBox = null;
 let copiedBox = null;
+let prevColor = null;
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        if (copiedBox) {
+            copiedBox = null;
+            boxes.forEach(box => {
+                box.classList.remove("copied");
+            });
+        }
+    }
+})
 
 boxes.forEach(box => {
-    box.addEventListener("dblclick", (event) => {
-        modal.classList.remove("hide-modal");
-        selectedBox = event.target;
-    });
-
     box.addEventListener("click", function(event) {
+        if (event.ctrlKey) {
+            modal.classList.remove("hide-modal");
+            selectedBox = event.target;
+            return;
+        }
+        
         if (!copiedBox) {
+            console.log(selectedBox)
             copiedBox = event.target;
+            boxes.forEach(box => {
+                box.classList.add("copied");
+            });
         }
         else {
             event.target.style.backgroundColor = copiedBox.style.backgroundColor;
-            copiedBox = null;
         }
+    });
+
+    box.addEventListener("dragstart", function(event) {
+        prevColor = window.getComputedStyle(this).backgroundColor;
+        event.target.dataset.dragging = "true";
+        event.target.classList.add("dragging");
+    });
+    box.addEventListener("dragleave", (event) => {
+        event.target.classList.remove("dragging");
+        event.target.dataset.dragging = "false";
+    });
+
+    box.addEventListener("dragover", (event) => {
+        event.preventDefault();
+        event.target.classList.add("hover");
+    });
+
+    box.addEventListener("dragleave", (event) => {
+        event.target.classList.remove("hover");
+    });
+
+    box.addEventListener("drop", (event) => {
+        event.target.classList.remove("hover");
+        event.target.style.backgroundColor = prevColor;
     });
 });
 
